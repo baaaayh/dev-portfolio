0fcdf56e08978c299367ae98029c4d325dbc8217
WIN11PRO\WIN11PRO <baaaayh@gmail.com>
2025-02-08 01:07:51 +0900

    updated.

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -3,7 +3,12 @@ import styles from "@/app/styles/layout/footer.module.scss";
 export default function Footer() {
     return (
         <footer className={styles["footer"]}>
-            <div className={styles["footer__inner"]}></div>
+            <div className={styles["footer__inner"]}>
+                <div className={styles["footer__top"]}></div>
+                <div className={styles["footer__bottom"]}>
+                    <div className={styles["footer__copyright"]}></div>
+                </div>
+            </div>
         </footer>
     );
 }
